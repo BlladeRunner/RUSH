afb32ebfdd4aec6360d05b9f2bf58dbde3434804
Rufus JS <rufatahmedik@gmail.com>
2021-05-03 23:59:38 +0200

    navigation toggle menu modifying

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const 
+const activeMenu = document.getElementByC('')
+const inactiveMenu = document.getElementById('')
+const navbar = document.getElementById('nav')
+
+activeMenu.addEventListener(click, toggle)
+
+function toggle() {
+    navbar.style.display = "none"
+}
